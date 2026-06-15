d99c9ec1ca5db1c53148a3d0827e928e1bb7fa55
manishayadhav <imanishayadav08@gmail.com>
2026-06-15 12:43:01 +0530

    code push

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "362261299" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1554926008" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
